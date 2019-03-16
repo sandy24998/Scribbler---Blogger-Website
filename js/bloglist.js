9d0dc80a373028ae403f8957736f6041f186c7f3
sandy24998 <24panditsandeep@gmail.com>
2019-03-16 09:11:00 +0530

    Comments and flex portion left only

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,4 +1,8 @@
 var delModal = document.getElementById("deleteModal");
+var noBtnInModal = document.getElementsByClassName("no-btn");
 function showDeleteModal(){
     delModal.style.display ="block";
+}
+function hideModal(){
+    delModal.style.display ="none"; 
 }
